98c0a5242cb71a24b5ef19b2a0b28200af46d6cb
Brian Wolf <wolfbj32@gmail.com>
2024-11-25 15:34:35 -0500

    Created app

---
+++ PortfolioWebsiteUpdated/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Toolbar />
+    <main>
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<script>
+import Toolbar from "@/components/Toolbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    Toolbar,
+  },
+};
+</script>
+
+<style></style>
---
+++ PortfolioWebsiteUpdated/src/components/Toolbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar :elevation="2">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/experience">Experience</RouterLink>
+      <RouterLink to="/projects">Projects</RouterLink>
+      <RouterLink to="/contact">Contact</RouterLink>
+    </nav>
+  </v-app-bar>
+</template>
+
+<script>
+import { RouterLink } from "vue-router";
+</script>
